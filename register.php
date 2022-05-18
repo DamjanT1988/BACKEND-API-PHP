e74f680a0c678f2b2dc992e4948b3b246c819003
Damjan Tosic <Damjan_t@hotmail.com>
2022-05-18 23:09:56 +0200

    done register

--- register.php
+++ register.php
@@ -3,26 +3,23 @@ $page_title = "Registrera";
 include("includes/header.php");
 ?>
 
+
 <h2>Registrera nedan!</h2>
 
 <!--create table-->
-<form action="store.php" method="POST">
-<label for="emailnew">Skriv in ditt arbetsmejl:</label>
+<form  name="myform" id="myform">
+
+<label for="namenew">Skriv in användarnamn:</label>
 <br>
-<input type="emailnew" name="emailnew" id="emailnew">
+<input type="text" name="namenew" id="namenew" placeholder="namn">
 <br>
 <label for="passwordnew">Välj ett starkt lösenord:</label>
 <br>
 <input type="password" name="passwordnew" id="passwordnew">
 <br>
-<label for="realnamener">Skriv in ditt namn:</label>
+<label for="employeeno">Skriv in anställdes anställningsnummer:</label>
 <br>
-<input type="realfnamenew" name="realfnamenew" id="realfnamenew" placeholder="förnamn">
-<input type="reallnamenew" name="reallnamenew" id="reallnamenew" placeholder="efternamn">
-<br>
-<label for="employeeno">Skriv in ditt anställningsnummer:</label>
-<br>
-<input type="employeeno" name="employeeno" id="employeeno">
+<input type="text" name="employeeno" id="employeeno">
 
 <br>
 <input type="submit" class="button1" value="Registrera användarkonto">
@@ -39,3 +36,26 @@ if(isset($_SESSION['errorlagring'])) {
 <?php
 include("includes/footer.php");
 ?>
+
+<script>
+//let formData = JSON.stringify($("#myform").serializeArray());
+
+let form = document.getElementById('myform');
+form.onsubmit = function(event){
+        let xhr = new XMLHttpRequest();
+        let formData = new FormData(form);
+        //open the request
+        xhr.open('POST','http://localhost/projekt_webservice_vt22-DamjanT1988/webservice-API.php?idregister=1')
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        //send the form data
+        xhr.send(JSON.stringify(Object.fromEntries(formData)));
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                form.reset(); //reset form after AJAX success or do something else
+            }
+        }
+        //Fail the onsubmit to avoid page refresh.
+        return false; }
+</script>
